1d2f38d9b9fe9b37eef9544ea38c449526bdc4ea
JCM <jcm@jcm.re>
2025-07-26 05:51:51 +0200

    allow putting device to sleep with power button

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,14 @@ struct ClockState {
     bool expanded = false;
 };
 
+void enter_sleep_mode() {
+    instance.lightSleep();
+
+    // after waking up, force a refresh and activity trigger (so we won't sleep again immediately)
+    lv_refr_now(nullptr);
+    lv_disp_trig_activity(nullptr);
+}
+
 void setup()
 {
     Serial.begin(115200);
@@ -35,7 +43,7 @@ void setup()
     lv_style_set_bg_color(&cyberpunk_style, lv_color_hex(0x0b0c16));
     lv_style_set_text_color(&cyberpunk_style, lv_color_hex(0xffffff));
 
-    lv_theme_t * th_act = lv_display_get_theme(NULL);
+    lv_theme_t * th_act = lv_display_get_theme(nullptr);
     static lv_theme_t cyberpunk_theme = *th_act;
     static auto theme_callback = [](lv_theme_t* th, lv_obj_t* obj){
         lv_obj_add_style(obj, &cyberpunk_style, 0);
@@ -43,7 +51,7 @@ void setup()
     lv_theme_set_parent(&cyberpunk_theme, th_act);
     lv_theme_set_apply_cb(&cyberpunk_theme, theme_callback);
 
-    lv_disp_set_theme(lv_disp_get_default(), &cyberpunk_theme);
+    lv_disp_set_theme(lv_display_get_default(), &cyberpunk_theme);
 
     lv_obj_add_style(lv_screen_active(), &cyberpunk_style, 0);
     ui_main = lv_tileview_create(lv_screen_active());
@@ -109,24 +117,30 @@ void setup()
             lv_obj_set_style_text_color(ui_battery_label, lv_color_hex(0xFFFFFF), 0);
         }
         lv_bar_set_value(ui_battery_bar, battery_percent, LV_ANIM_OFF);
-    }, 1000, NULL);
+    }, 1000, nullptr);
     lv_timer_create([](lv_timer_t* timer){
-        uint32_t inactive_time = lv_display_get_inactive_time(NULL);
+        uint32_t inactive_time = lv_display_get_inactive_time(nullptr);
         if(inactive_time > 30000) {
-            instance.lightSleep();
+            enter_sleep_mode();
         } else if(inactive_time > 10000) {
             instance.setBrightness(1);
         } else {
             instance.setBrightness(DEVICE_MAX_BRIGHTNESS_LEVEL);
         }
-    }, 1000, NULL);
+    }, 1000, nullptr);
 
     instance.setBrightness(DEVICE_MAX_BRIGHTNESS_LEVEL);
     instance.enableCharge(1000);
+    instance.onEvent([](DeviceEvent_t event, void* user_data) {
+        if (event == PMU_EVENT_KEY_CLICKED) {
+            enter_sleep_mode();
+        }
+    }, nullptr);
 }
 
 void loop()
 {
+    instance.loop();
     lv_timer_handler();
     delay(2);
 }
